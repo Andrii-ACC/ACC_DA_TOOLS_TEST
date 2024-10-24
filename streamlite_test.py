cee945d39843c4e98b5078b96c1d5c49b1116f11
Andrii-ACC <andrii@acceleratedagency.com>
2024-10-24 12:37:40 +0300

    first eng commit

--- streamlite_test.py
+++ streamlite_test.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.title("Подсчет продаж")
+st.title("Calculate of sales")
 
 # Ввод данных пользователем
-price = st.number_input("Цена товара", min_value=0.0, step=0.01)
-quantity = st.number_input("Количество проданных единиц", min_value=0)
+price = st.number_input("Product price", min_value=0.0, step=0.01)
+quantity = st.number_input("Amounbt of sales", min_value=0)
 
 # Вычисление результата
 total = price * quantity
 
 # Кнопка для запуска вычислений
-if st.button("Рассчитать"):
-    st.success(f"Общая сумма продаж: {total} grn.")
+if st.button("Calculate"):
+    st.success(f"Total amount: {total} $.")
