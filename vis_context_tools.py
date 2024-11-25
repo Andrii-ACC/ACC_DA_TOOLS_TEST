97b9ad6946dc04f9b130e8cabfafffee7d871720
Andrii-ACC <andrii@acceleratedagency.com>
2024-11-25 13:35:52 +0200

    add VisContxt tool

--- vis_context_tools.py
+++ vis_context_tools.py
@@ -14,7 +14,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+
+
 
 
 
